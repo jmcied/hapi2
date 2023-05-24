c4262e9176b4b3de3e06835e691e7681d7cff62b
John McDonald <96137405+jmcied@users.noreply.github.com>
2023-05-24 20:44:03 +0100

    .env fix

--- src/server.js
+++ src/server.js
@@ -47,8 +47,8 @@ async function init() {
 
   server.auth.strategy("session", "cookie", {
     cookie: {
-      name: process.env.cookie.name,
-      password: process.env.cookie.password,
+      name: process.env.cookie_name,
+      password: process.env.cookie_password,
       isSecure: false,
     },
     redirectTo: "/",
